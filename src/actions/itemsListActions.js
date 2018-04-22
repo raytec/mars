5db657ccffb3f1ce975d4866615c5888c8c149b8
Red <red@Reds-MacBook-Pro.local>
2018-04-22 21:48:23 +0200

    Flow typing

--- src/actions/itemsListActions.js
+++ src/actions/itemsListActions.js
@@ -1,21 +1,26 @@
+// @flow
 import { ITEMS_LIST_REQUEST} from './actionTypes';
 import { ITEM_DETAILS_REQUEST} from './actionTypes';
 import { ITEMS_LIST_INSTANT_FILTER_REQUEST} from './actionTypes';
 
-export const showListAction = () => {
+type ShowListAction = {type: string};
+type ShowItemDetailsAction = {type: string; selectedItem: number};
+type ApplyItemsListInstantFilterAction = {type: string; search: string};
+
+export const showListAction = () : ShowListAction => {
     return {
         type: ITEMS_LIST_REQUEST
     }
 }
 
-export const showItemDetailsAction = (item) => {
+export const showItemDetailsAction = (item : number) : ShowItemDetailsAction => {
     return {
         type: ITEM_DETAILS_REQUEST,
         selectedItem: item
     }
 }
 
-export const applyItemsListInstantFilterAction = (search) => {
+export const applyItemsListInstantFilterAction = (search : string) : ApplyItemsListInstantFilterAction => {
     return {
         type: ITEMS_LIST_INSTANT_FILTER_REQUEST,
         search: search
--- src/components/ItemDetailsComponent.js
+++ src/components/ItemDetailsComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, Text, View, Image} from 'react-native';
+import {Dimensions, Text, View, Image} from 'react-native';
 
 export default class ItemDetailsComponent extends React.Component {
 
